fa1da87ed477025f6772b0982921cb5ac1cbb070
Karunakaran KS <paradox@outlook.in>
2016-11-07 18:04:31 +0530

    Add files via upload

---
+++ src/test/java/tests/PostsTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+import static org.hamcrest.Matchers.greaterThan;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PostsTest extends RAConfig {
+
+	private static final Logger LOGGER = Logger.getLogger(PostsTest.class.getName());
+    
+	@Test
+	public void getPosts() {
+		get("/posts").then().assertThat().body("size()", greaterThan(10)).body("id", hasItems(1, 2, 3));
+		LOGGER.info(String.format("get::posts verified"));	
+	}
+
+	@Test
+	public void shouldRetrievePost() {
+		getPostTest(1);
+		getPostTest(2);
+		getPostTest(3);
+	}
+
+	public void getPostTest(int id) {
+		get("/posts/{id}", id).then().assertThat().body("id", equalTo(id)).body("title", notNullValue())
+				.body("body", notNullValue()).body("userId", notNullValue());
+		LOGGER.info(String.format("get::post [%s] verified", id));		
+	}
+	@Test
+	public void createPost() {
+		Map<String,String> post = new HashMap<>();
+        post.put("title", "RA tst Post");
+        post.put("body", "RA tst Post");
+        post.put("userId", "13");
+
+       String id= given()
+        .contentType("application/json")
+        .body(post)
+        .when().post("/posts").then().statusCode(201).extract().body().jsonPath().get("id").toString();
+		LOGGER.info(String.format("post::post [%s] created",id));		
+	}
+}
---
+++ src/test/java/tests/RAConfig.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.junit.rules.Timeout;
+import io.restassured.RestAssured;
+
+public abstract class RAConfig {
+
+	static{
+		 System.setProperty("java.util.logging.SimpleFormatter.format", 
+		            "%5$s%6$s%n");
+	}
+    public static final String REST_URI = "http://jsonplaceholder.typicode.com/";
+
+    @Rule public Timeout globalTimeout = new Timeout(200000);
+    @Rule public TestName name = new TestName();
+
+    @Before
+    public void setUp() {
+        System.out.println("--> "+name.getMethodName());
+    }
+
+    @BeforeClass
+    public static void setUpCaseFlow() {
+    	RestAssured.proxy("proxy.cognizant.com",6050);
+    	RestAssured.proxy.withAuth("389747", "g0tch@..1");
+        RestAssured.useRelaxedHTTPSValidation();
+        RestAssured.baseURI=REST_URI;
+    }
+
+}
